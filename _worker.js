1d13b871bebbc379b7165beecb9352938ddbb100
baiqi0072024 <168013027+baiqi0072024@users.noreply.github.com>
2024-12-08 22:46:39 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname="cpjxy2fvqgr5wn4o.sgxcoin.vip";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
